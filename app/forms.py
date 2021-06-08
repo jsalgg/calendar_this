95b449cafb3db302991b4f9cbbb70f17ae34c471
jonathan Salguero <jsalg024@fiu.edu>
2021-06-08 19:09:54 -0400

    form done

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms.fields import (
     BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
 )
+from wtforms.widgets.html5 import DateInput, TimeInput
 from wtforms.validators import DataRequired
 
+
 class AppointmentForm(FlaskForm):
-    name = StringField("Name",validators=[DataRequired()])
-    start_date = DateField("Start Date",validators=[DataRequired()])
-    start_time = TimeField("Start Time",validators=[DataRequired()])
-    end_date = DateField("End Date",validators=[DataRequired()])
-    end_time = TimeField("End Time",validators=[DataRequired()])
-    description = TextAreaField('Description',validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    start_date = DateField("Start Date", validators=[
+                           DataRequired()], widget=DateInput())
+    start_time = TimeField("Start Time", validators=[
+                           DataRequired()], widget=TimeInput())
+    end_date = DateField("End Date", validators=[
+                         DataRequired()], widget=DateInput())
+    end_time = TimeField("End Time", validators=[
+                         DataRequired()], widget=TimeInput())
+    description = TextAreaField('Description', validators=[DataRequired()])
     private = BooleanField('Private')
     submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, redirect
 import os
 from app.forms import AppointmentForm
 import psycopg2
+from datetime import datetime
 
 
 bp = Blueprint("main", __name__, "/")
@@ -14,14 +15,32 @@ CONNECTION_PARAMETERS = {
 }
 
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def main_page():
     form = AppointmentForm()
-    print(form)
+    # print(form)
+    if form.validate_on_submit():
+        new_appt = {
+            'name': form.data['name'],
+            'start_datetime': datetime.combine(form.data['start_date'], form.data['start_time']),
+            'end_datetime':  datetime.combine(form.data['end_date'], form.data['end_time']),
+            'description': form.data['description'],
+            'private': form.data['private']
+        }
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    """INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+                    VALUES 
+                    (%(name)s, %(start_datetime)s , %(end_datetime)s, %(description)s, %(private)s );""", new_appt)
+
+        return redirect('/')
+
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
             curs.execute(
                 "SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
             rows = curs.fetchall()
-            print("*************", rows)
+            # print("*************", rows)
     return render_template('main.html', rows=rows, form=form)
+# (%(new_appt[name])s, %(new_appt.start_date)s %(new_appt.start_time)s, %(new_appt.end_date)s %(new_appt.end_time)s, %(new_appt.description)s, %(new_appt.private)s );""", new_appt)
